fb79f6bd74747c7e730400880e5c0fc9da602706
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-04-30 14:27:15 +0530

    Updated html css js and complete the logic part

--- Image Gallery/script.js
+++ Image Gallery/script.js
@@ -1,33 +1,52 @@
-const img2 = document.querySelector('.img-2');
-const img1 = document.querySelector('.img-1');
-const img3 = document.querySelector('.img-3');
-const img4 = document.querySelector('.img-4');
+const img2 = document.querySelector(".img-2");
+const img1 = document.querySelector(".img-1");
+const img3 = document.querySelector(".img-3");
+const img4 = document.querySelector(".img-4");
 
+img2.addEventListener("click", () => {
+  img2.style.width = "70%";
+  img1.style.width = "10%";
+  img3.style.width = "10%";
+  img4.style.width = "10%";
+  img2.style.transition = " ease-in-out 1.4s ";
+});
+img1.addEventListener("click", () => {
+  img1.style.width = "70%";
+  img2.style.width = "10%";
+  img3.style.width = "10%";
+  img4.style.width = "10%";
+  img1.style.transition = " ease-in-out 1.4s ";
+});
+img3.addEventListener("click", () => {
+  img3.style.width = "70%";
+  img4.style.width = "10%";
+  img2.style.width = "10%";
+  img1.style.width = "10%";
+  img3.style.transition = " ease-in-out 1.4s ";
+});
+img4.addEventListener("click", () => {
+  img4.style.width = "70%";
+  img4.style.transition = " ease-in-out 1.4s ";
+  img3.style.width = "10%";
+  img2.style.width = "10%";
+  img1.style.width = "10%";
+});
 
-img2.addEventListener('click',()=>{
-    // img2.style.width = '70%';
-    // img1.style.width = '10%';
-    // img3.style.width = '10%';
-    // img4.style.width = '10%';
-   img2.classList.add('active');
+// const gal = document.querySelectorAll('.panel');
 
-})
-img1.addEventListener('click',()=>{
-    img1.style.width = '70%';
-    img2.style.width = '10%'
-    img3.style.width = '10%';
-    img4.style.width = '10%'
-})
-img3.addEventListener('click',()=>{
-    img3.style.width = '70%';
-    img4.style.width = '10%'
-    img2.style.width = '10%';
-    img1.style.width = '10%';
-})
-img4.addEventListener('click',()=>{
-    img4.style.width = '70%';
+// gal.forEach((e)=>{
+//     e.addEventListener('click',()=>{
+//         removeClass()
+//         e.classList.add('active');
+//         console.log('hey')
+//     })
 
-    img3.style.width = '10%'
-    img2.style.width = '10%';
-    img1.style.width = '10%';
-})
+// })
+
+// function removeClass(){
+//     gal.forEach((e)=>{
+//         e.addEventListener('click',()=>{
+//             e.classList.remove('active');
+//         })
+//     })
+// }
